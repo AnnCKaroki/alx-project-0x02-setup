90bea16e62ad48d758ac8fbe8ed2e9da65e919e8
Ann <annkarokiadmissions@gmail.com>
2025-07-27 04:22:07 +0300

    Add modal component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import Card from "@/components/common/Card";
+import React from 'react';
+import {useState} from 'react';
+import Card from "@/components/common/Card";;
+import PostModal from '@/components/common/PostModal';
+import { CardProps } from '@/interfaces';
+
 
 const HomePage: React.FC =() => {
+
+     const [posts, setPosts] = useState<CardProps[]>([
+    {
+      title: 'Welcome to ALX',
+      content: 'This project demonstrates reusable components using Next.js and TypeScript.',
+    },
+  ]);
+
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddPost = (newPost: CardProps) => {
+    setPosts((prev) => [...prev, newPost]);
+  };
+
   return(
     <>
     <h1>Welcome</h1>
     <Card title="Learn by doing" content="Do hard things!" />
+
+    <div className="space-y-4 w-full flex flex-col items-center">
+        {posts.map((post, index) => (
+          <Card key={index} title={post.title} content={post.content} />
+        ))}
+      </div>
+
+    <PostModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSubmit={handleAddPost}
+      />
     </>
   )
 }
