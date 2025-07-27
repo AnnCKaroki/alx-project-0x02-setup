e83b686cab2f87ec95cbb547e33cd08ad569d3ac
Ann <annkarokiadmissions@gmail.com>
2025-07-27 03:39:05 +0300

    Create Reusable Card Component

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Card from '../components/common/Card'
 
 const HomePage: React.FC =() => {
   return(
+    <>
     <h1>Welcome User!</h1>
+    <Card title="Learn by doing" content="Do hard things!" />
+    </>
   )
 }
 
